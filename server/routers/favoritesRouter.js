68e5f2601820faedc54f301d6dfb92d085e6f5d6
Elizabeth Wong <lizzwong@gmail.com>
2018-03-04 13:27:13 -0600

    server router and index

---
+++ server/routers/favoritesRouter.js
@@ -0,0 +1,59 @@
+const exprss = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+
+const FavoriteSchema = new mongoose.Schema(
+    {
+        name: {type: String, required: true},
+        url: String,
+        }
+)
+
+const Favorite = mongoose.model('Favorite', FavoriteSchema, 'swapi' );
+
+
+router.get('/', (request,response) =>{
+    Favorite.find({}, (error, foundFavorites)=>{
+        if (error){
+            console.log('Error on finding Favorites:', error);
+           response.sendStatus(500); 
+        }
+        else {
+            console.log('Favorites found');
+            response.send(foundFavorites);
+        }
+    })
+});
+
+router.post('/', (request, response) => {
+    let newFavorite = new Favorite (request.body);
+    console.log('New favorite is:', request.body);
+    newFavorite.save((error, savedFavorite) => {
+        if (error) {
+            console.log('error adding Favorite', error);
+            response.sendStatus(500);
+        }
+        else {
+            console.log('Favorite added');
+            response.sendStatus(201);
+        }
+    })
+});
+
+router.delete('/delete/:id', (request, response)=>{
+    let id = request.params.id;
+    console.log('Favorite to delete is', resquest.body);
+    Facorite.findByIdAndRemove(
+        {"_id":id},
+        (error, deleteEmployee) =>{
+            if (error){
+                console.log('Error on delete Favorite',error);
+                response.sendStatus(500);  
+            }
+            else {
+                response.sendStatus(200);
+            }
+    })
+})
+
+module.exports = router;
---
+++ server/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+appl.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(express.static('server/public'));
+
+//mongoose
+const mongoose = require('mongoose');
+
+const databaseUrl = 'mongodb://localhost27017/favorites';
+
+mongoose.connection.on('connected', function (){
+    console.log('mongoose conntected to:', databaseUrl);    
+})
+
+mongoose.connection.on('error', function(error){
+    console.log('mongoose connection error', error);   
+})
+
+mongoose.connect(databaseUrl);
+//end mongoose setup
+
+//router
+const favoritesRouter = require('.routers/favoritesRouter');
+app.use('/favorites', favoritesRouter);
+
+//server
+let port = process.env.PORT || 5000;
+
+app.listen(port, function(){
+    console.log('listening on port:', port);
+    
+})
